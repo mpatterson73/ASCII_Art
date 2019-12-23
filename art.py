61e82eea5818857adc88cf3d27c9c942f97caab8
Mike Patterson <mpatterson73@gmail.com>
2019-12-23 15:35:10 -0500

    verify thumbnail size and matrix size

--- art.py
+++ art.py
@@ -5,13 +5,16 @@ import numpy as np
 def create_matrix(image):
     image.thumbnail((400, 600))
     # image.show()
+    print(f'Thumbnail size: {image.size}')
     pixel_data = list(image.getdata())
-    for i in range(0, len(pixel_data), image.width):
-        pixel_matrix = pixel_data[i:image.width]
+    #for i in range(0, len(pixel_data), image.width):
+    #    pixel_matrix = pixel_data[i:image.width]
 
     pixel_matrix = [pixel_data[i:i+image.width]
                     for i in range(0, len(pixel_data), image.width)]
-    return pixel_matrix
+    print('Successfully constructed pixel matrix: ')
+    print(f'Pixel matrix size: {len(pixel_matrix[0])} x {len(pixel_matrix)}')
+    return #pixel_matrix
 
 
 with open('Photos/grier_cat.jpeg', 'rb') as fp:
